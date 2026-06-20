3489fb5bff376a947d6f949a11ddf353c64e5fee
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-06-19 22:03:28 -0400

    Crea AppModule.kt

---
+++ app/src/main/java/edu/ucne/tarea4apiplanets/di/AppModule.kt
@@ -0,0 +1,42 @@
+package edu.ucne.tarea4apiplanets.di
+
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import edu.ucne.tarea4apiplanets.data.remote.DragonBallApi
+import edu.ucne.tarea4apiplanets.data.repository.PlanetRepositoryImpl
+import edu.ucne.tarea4apiplanets.domain.repository.PlanetRepository
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi{
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+    @Provides
+    @Singleton
+    fun provideApi(moshi: Moshi): DragonBallApi{
+        return Retrofit.Builder()
+            .baseUrl("https://dragonball-api.com/api/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(DragonBallApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(api: DragonBallApi): PlanetRepository{
+        return PlanetRepositoryImpl(api)
+    }
+}
