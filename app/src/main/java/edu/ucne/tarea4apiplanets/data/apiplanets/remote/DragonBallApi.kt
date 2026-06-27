23f8a35a466fb0e94acd008af8b69f8bcc44438a
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-06-27 19:30:30 -0400

    Modifica DragonBallApi.kt

--- app/src/main/java/edu/ucne/tarea4apiplanets/data/apiplanets/remote/DragonBallApi.kt
+++ app/src/main/java/edu/ucne/tarea4apiplanets/data/apiplanets/remote/DragonBallApi.kt
@@ -1,5 +1,7 @@
 package edu.ucne.tarea4apiplanets.data.apiplanets.remote
 
+import edu.ucne.tarea4apiplanets.data.apiplanets.remote.dto.CharacterDto
+import edu.ucne.tarea4apiplanets.data.apiplanets.remote.dto.CharacterResponseDto
 import edu.ucne.tarea4apiplanets.data.apiplanets.remote.dto.PlanetDto
 import edu.ucne.tarea4apiplanets.data.apiplanets.remote.dto.PlanetsResponseDto
 import retrofit2.Response
@@ -20,4 +22,18 @@ interface DragonBallApi{
     suspend fun getPlanetDetail(
         @Path("id") id: Int
     ): Response<PlanetDto>
+
+    @GET("characters")
+    suspend fun getCharacters(
+        @Query("page") page: Int,
+        @Query("limit") limit: Int,
+        @Query("name") name: String?,
+        @Query("gender") gender: String?,
+        @Query("race") race: String?
+    ): Response<CharacterResponseDto>
+
+    @GET("characters/{id}")
+    suspend fun getCharacterDetail(
+        @Path("id") id: Int
+    ): Response<CharacterDto>
 }
